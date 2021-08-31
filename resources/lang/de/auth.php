7a0885a1bfa24b8cc217b66e5f8c15b39cdee4b3
JanKrb <jankrb@jankrb.com>
2021-08-31 20:39:45 +0200

    Translate auth

--- resources/lang/de/auth.php
+++ resources/lang/de/auth.php
@@ -1,19 +1,18 @@
 
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Authentication Language Lines
-|--------------------------------------------------------------------------
-|
-| The following language lines are used during authentication for various
-| messages that we need to display to the user. You are free to modify
-| these language lines according to your application's requirements.
-|
-*/
-
 return [
     'failed'   => 'Diese Kombination aus Zugangsdaten wurde nicht in unserer Datenbank gefunden.',
     'password' => 'Das eingegebene Passwort ist nicht korrekt.',
     'throttle' => 'Zu viele Loginversuche. Versuchen Sie es bitte in :seconds Sekunden nochmal.',
+    'login_success' => 'Erfolgreich eingeloggt.',
+    'login_failed' => 'Anmelden fehlgeschlagen.',
+    'register_success' => 'Account erfolgreich erstellt.',
+    'unauthenticated' => 'Benutzer ist nicht angemeldet.',
+    'already_authed' => 'Benutzer ist bereits angemeldet.',
+    'get_details' => 'Erfolgreich Benutzerdetails geladen.',
+    'logged_out' => 'Erfolgreich vom Konto abgemeldet.',
+    'invalid_email' => 'Diese Email-Adresse gehört zu keinem Account.',
+    'invalid_reset_token' => 'Ungültiger Zugangsschlüssel.',
+    'invalid_password' => 'Ungültiges Passwort.'
 ];
