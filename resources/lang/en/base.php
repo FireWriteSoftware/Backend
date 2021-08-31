52020f5428f0cc75d9630cb878f35147344e782f
JanKrb <jankrb@jankrb.com>
2021-08-31 13:46:13 +0200

    Translate relation base controller

---
+++ resources/lang/en/base.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'base' => [
+
+    ],
+
+    'relation' => [
+        'invalid_parent' => 'Invalid parent submitted.',
+        'invalid_child' => 'Invalid child submitted.',
+        'get_all_success' => 'Successfully retrieved relation data.',
+        'checked_success' => 'Successfully verified relationship of items.',
+        'attached_success' => 'Successfully attached child to parent.',
+        'detached_success' => 'Successfully detached child from parent.',
+    ],
+];
