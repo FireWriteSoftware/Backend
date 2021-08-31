dbd6aa2c5ec8b9cc4b55c2b6aed16a3d6fc92455
JanKrb <jankrb@jankrb.com>
2021-08-31 17:09:27 +0200

    Translate post comments

--- resources/lang/en/ban.php
+++ resources/lang/en/ban.php
@@ -2,4 +2,6 @@
 
 return [
     'is_global' => 'User is globally banned.',
+    'is_comment' => 'User is banned from comments.',
+    'comment_ban_reason' => 'You need a verified email address to write a comment'
 ];
