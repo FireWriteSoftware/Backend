de6b5c561642dd28cdeb5166aea6549c368cfc91
JanKrb <jankrb@jankrb.com>
2021-09-01 19:29:09 +0200

    Translate mail

---
+++ resources/lang/de/post.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'mail' => [
+        'post_created' => 'Ein Post aus einer gespeicherten Kategorie wurde erstellt.',
+        'action' => ':title von :name',
+        'link' => 'Zum Post',
+        'call_to_vote' => 'Lasse gerne Feedback in Form eines Gefällt mir / Gefällt mir nicht da.'
+    ]
+];
