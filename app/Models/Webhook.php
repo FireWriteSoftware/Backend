e91ad92a41542027a294bf873a97c15b8ac3b83c
JanKrb <jankrb@jankrb.com>
2021-09-07 20:11:34 +0200

    Add notification route helper

--- app/Models/Webhook.php
+++ app/Models/Webhook.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class Webhook extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Notifiable;
 
     protected $fillable = [
         'type',
@@ -19,5 +20,14 @@ class Webhook extends Model
     public function scopes(): HasMany
     {
         return $this->hasMany(WebhookScope::class);
+    }
+
+    public function routeNotificationForDiscord(): string
+    {
+        if ($this->type === 'discord') {
+            return $this->url;
+        }
+
+        return '';
     }
 }
